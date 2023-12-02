82c1e4e1faa61c17507e8639eb1d376ba1f947d8
Damian Polchlopek <damian.polchlopek1@gmail.com>
2023-12-02 21:08:25 +0100

    Refactor Exceptions

--- Backend/src/main/java/com/improvement_app/workouts/exceptions/TrainingExceptionsHandler.java
+++ Backend/src/main/java/com/improvement_app/workouts/exceptions/TrainingExceptionsHandler.java
@@ -12,15 +12,35 @@ public class TrainingExceptionsHandler {
     @ResponseBody
     @ExceptionHandler(ExercisesNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String exerciseNotFoundHandler(ExercisesNotFoundException ex) {
+    public String exerciseNotFoundExceptionHandler(ExercisesNotFoundException ex) {
         return ex.getMessage();
     }
-
 
     @ResponseBody
     @ExceptionHandler(TrainingTemplateNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String trainingTemplateNotFoundHandler(TrainingTemplateNotFoundException ex) {
+    public String trainingTemplateNotFoundExceptionHandler(TrainingTemplateNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ExerciseTypeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String exerciseTypeNotFoundExceptionHandler(ExerciseTypeNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(FileNotCreatedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String fileNotCreatedExceptionHandler(FileNotCreatedException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(FileNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String fileNotFoundExceptionHandler(FileNotFoundException ex) {
         return ex.getMessage();
     }
 }
