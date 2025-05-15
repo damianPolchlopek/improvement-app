2961234a00a7809260b51a1d932bea007310f540
Damian Polchlopek <damian.polchlopek1@gmail.com>
2025-05-15 18:23:13 +0200

    Refactor DockerFile and fix backend warinings

--- Backend/src/main/java/com/improvement_app/ImprovementAppBackendApplication.java
+++ Backend/src/main/java/com/improvement_app/ImprovementAppBackendApplication.java
@@ -2,9 +2,22 @@ package com.improvement_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "com.improvement_app.food.infrastructure",
+        "com.improvement_app.security.repository",
+        "com.improvement_app.parser.repository",
+})
+@EnableMongoRepositories(basePackages = {
+        "com.improvement_app.other.weekly.repository",
+        "com.improvement_app.other.daily.repository",
+        "com.improvement_app.workouts.repository",
+        "com.improvement_app.shopping.repository",
+})
 public class ImprovementAppBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImprovementAppBackendApplication.class, args);
