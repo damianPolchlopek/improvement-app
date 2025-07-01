6139de71176d87f976e8ac9b1264b6896254b63d
Damian Polchlopek <damian.polchlopek1@gmail.com>
2025-07-01 17:47:34 +0200

    Restart Exercise Name Table

---
+++ Backend/src/main/resources/db/changelog/0012_restart_exercise_name_table/001.restart_exercise_name.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS workout.exercise_name CASCADE;
+
+CREATE TABLE workout.exercise_name (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
