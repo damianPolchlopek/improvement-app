84864f99c6b2fcd9d6e24e24ec9263d1b68f9ffa
Damian Polchlopek <damian.polchlopek1@gmail.com>
2025-07-01 21:24:47 +0200

    Fix liquibase

--- Backend/src/main/resources/db/changelog/0012_restart_exercise_name_table/001.restart_exercise_name.sql
+++ Backend/src/main/resources/db/changelog/0012_restart_exercise_name_table/001.restart_exercise_name.sql
@@ -1,7 +1,5 @@
--- Usuń istniejącą tabelę
 DROP TABLE IF EXISTS workout.exercise_name CASCADE;
 
--- Utwórz tabelę z poprawną definicją IDENTITY
 CREATE TABLE workout.exercise_name (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
