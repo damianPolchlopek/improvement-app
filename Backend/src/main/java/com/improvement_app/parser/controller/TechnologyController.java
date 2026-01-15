f04c6803cfcf5d7df79b094371534020ae8a4797
Damian Polchlopek <damian.polchlopek1@gmail.com>
2026-01-15 18:36:10 +0100

    Comment parser

--- Backend/src/main/java/com/improvement_app/parser/controller/TechnologyController.java
+++ Backend/src/main/java/com/improvement_app/parser/controller/TechnologyController.java
@@ -1,27 +1,27 @@
-package com.improvement_app.parser.controller;
-
-import com.improvement_app.parser.service.TechnologyService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-
-
-@RestController
-@RequiredArgsConstructor
-//@EnableScheduling
-public class TechnologyController {
-
-    private final TechnologyService technologyService;
-
-    @GetMapping("/technology")
-//    @Scheduled(cron = "0 0 20 * * ?") // Uruchamia się codziennie o 4:00 rano
-    private void addNewJavaOffer() throws IOException {
-        System.out.println("[DPO] Uruchomilem sie");
-        technologyService.parseAllJobOffers();
-    }
-
-}
+//package com.improvement_app.parser.controller;
+//
+//import com.improvement_app.parser.service.TechnologyService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.IOException;
+//
+//
+//@RestController
+//@RequiredArgsConstructor
+////@EnableScheduling
+//public class TechnologyController {
+//
+//    private final TechnologyService technologyService;
+//
+//    @GetMapping("/technology")
+////    @Scheduled(cron = "0 0 20 * * ?") // Uruchamia się codziennie o 4:00 rano
+//    private void addNewJavaOffer() throws IOException {
+//        System.out.println("[DPO] Uruchomilem sie");
+//        technologyService.parseAllJobOffers();
+//    }
+//
+//}
--- Backend/src/main/java/com/improvement_app/parser/entity/TechnologyList.java
+++ Backend/src/main/java/com/improvement_app/parser/entity/TechnologyList.java
@@ -1,34 +1,34 @@
-package com.improvement_app.parser.entity;
-
-import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Type;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "technology_list", schema = "technologies")
-public class TechnologyList {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private LocalDate date;
-
-    @Type(JsonType.class)
-    @Column(columnDefinition = "jsonb")
-    List<Technology> technologyList;
-
-    public TechnologyList(List<Technology> technologies) {
-        this.date = LocalDate.now();
-        this.technologyList = technologies;
-    }
-}
+//package com.improvement_app.parser.entity;
+//
+//import io.hypersistence.utils.hibernate.type.json.JsonType;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.annotations.Type;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Data
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Table(name = "technology_list", schema = "technologies")
+//public class TechnologyList {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private LocalDate date;
+//
+//    @Type(JsonType.class)
+//    @Column(columnDefinition = "jsonb")
+//    List<Technology> technologyList;
+//
+//    public TechnologyList(List<Technology> technologies) {
+//        this.date = LocalDate.now();
+//        this.technologyList = technologies;
+//    }
+//}
