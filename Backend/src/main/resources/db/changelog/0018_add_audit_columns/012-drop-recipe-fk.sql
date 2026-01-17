d2a1cb70c9d96d98ca6c951d483d2c23088555dc
Damian Polchlopek <damian.polchlopek1@gmail.com>
2026-01-17 14:00:15 +0100

    Fix meal init

---
+++ Backend/src/main/resources/db/changelog/0018_add_audit_columns/012-drop-recipe-fk.sql
@@ -0,0 +1,2 @@
+ALTER TABLE food.daily_meal
+    DROP CONSTRAINT IF EXISTS fk_daily_meal_recipe;
---
+++ Backend/src/main/resources/db/changelog/0018_add_audit_columns/013-drop-meal-recipe-fk.sql
@@ -0,0 +1,2 @@
+ALTER TABLE food.daily_meal_ingredient
+    DROP CONSTRAINT IF EXISTS fk_daily_meal_ingredient_product;
