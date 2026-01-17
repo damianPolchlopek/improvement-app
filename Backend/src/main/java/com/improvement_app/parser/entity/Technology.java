492cd25b6dd7bdcf4df7e1bad7023e46e95218c2
Damian Polchlopek <damian.polchlopek1@gmail.com>
2026-01-17 15:08:26 +0100

    Uncomment Technology

--- Backend/src/main/java/com/improvement_app/parser/entity/Technology.java
+++ Backend/src/main/java/com/improvement_app/parser/entity/Technology.java
@@ -1,20 +1,20 @@
-//package com.improvement_app.parser.entity;
-//
-//
-//import lombok.Data;
-//
-//@Data
-//public class Technology {
-//
-//    private String name;
-//
-//    private int occurrences;
-//
-//    @Override
-//    public String toString() {
-//        return "Technology{" +
-//                "name='" + name + '\'' +
-//                ", occurrences=" + occurrences +
-//                '}';
-//    }
-//}
+package com.improvement_app.parser.entity;
+
+
+import lombok.Data;
+
+@Data
+public class Technology {
+
+    private String name;
+
+    private int occurrences;
+
+    @Override
+    public String toString() {
+        return "Technology{" +
+                "name='" + name + '\'' +
+                ", occurrences=" + occurrences +
+                '}';
+    }
+}
--- Backend/src/main/java/com/improvement_app/parser/repository/TechnologyListRepo.java
+++ Backend/src/main/java/com/improvement_app/parser/repository/TechnologyListRepo.java
@@ -1,10 +1,10 @@
-//package com.improvement_app.parser.repository;
-//
-//import com.improvement_app.parser.entity.TechnologyList;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface TechnologyListRepo extends JpaRepository<TechnologyList, Long> {
-//
-//}
+package com.improvement_app.parser.repository;
+
+import com.improvement_app.parser.entity.TechnologyList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechnologyListRepo extends JpaRepository<TechnologyList, Long> {
+
+}
