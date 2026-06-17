19c949f88b62016d36ce0ff21722372119e92a8d
Damian Polchlopek <36795598+damianPolchlopek@users.noreply.github.com>
2026-06-17 23:32:01 +0200

    Configure vitest

--- frontend5/vite.config.js
+++ frontend5/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+
 
 export default defineConfig({
   plugins: [
@@ -19,5 +20,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    css: true,
   },
 });
